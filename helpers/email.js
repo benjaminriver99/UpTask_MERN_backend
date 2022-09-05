82b70a3efc074daf598c9c0b21715c007ea45de8
unknown <benjaminbenitez1999@hotmail.com>
2022-09-05 10:36:34 -0300

    changes updated

--- helpers/email.js
+++ helpers/email.js
@@ -1,66 +1,61 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export const emailRegistro = async (datos) => {
-  const { email, nombre, token} =  datos;
+  const { email, nombre, token } = datos;
 
-
-  // Datos del servidor del correo
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_HOST,
-    secure: false,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-        }
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
-  // Informacion del Email
+  // Información del email
+
   const info = await transport.sendMail({
-    from: ' "UpTask - Administrador de Proyectos Balanceados <bbenjamin@balanceados-sa.com.ar>',
-    to: email, // Envia al correo que pusimos en el formulario
-        subject: "Uptask - Comprobá tu cuenta", // Esto es lo visibile del correo
-        text: "Comprobá tu cuenta en Uptask", // Texto versión corta
-        html: ` 
-        <p>Hola: ${nombre} comprobá tu cuenta en UpTask Balanceados </p>
-        <p>Tu cuenta ya casi está lista, solamente tenés que comprobarla en el siguiente enlace:</p>
-
-        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
-
-        <p>Si no creaste esta cuenta, podés ignorar este mensaje.</p>
-        `
-  })
-
-}
+    from: '"UpTask - Administrador de Proyectos" <bbenjamin@balanceados-sa.com.ar>',
+    to: email,
+    subject: "UpTask - Comprobá tu cuenta",
+    text: "Comprobá tu cuenta en UpTask Balanceados",
+    html: `<p>Hola: ${nombre} Comprobá tu cuenta en UpTask</p>
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace: 
+    <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a>
+    
+    <p>Si no creaste esta cuenta, podés ignorar el mensaje</p>
+    
+    
+    `,
+  });
+};
 
 export const emailOlvidePassword = async (datos) => {
-  const { email, nombre, token} =  datos;
+  const { email, nombre, token } = datos;
 
-  // Datos del servidor del correo
   const transport = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_HOST,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-      },
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
-  // Informacion del Email
+  // Información del email
+
   const info = await transport.sendMail({
-    from: ' "UpTask - Administrador de Proyectos Balanceados <bbenjamin@balanceados-sa.com.ar>',
-    to: email, // Envia al correo que pusimos en el formulario
-        subject: "Uptask - Reestablece tu password", // Esto es lo visibile del correo
-        text: "Reestablece tu password", // Texto versión corta
-        html: ` 
-        <p>Hola: ${nombre}! Solicitaste reestablecer tu password </p>
-        <p>Sigue el siguiente enlace para generar un nuevo password:</p>
-
-        <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Contraseña</a>
-
-        <p>Si no solicitaste este correo, podés ignorar este mensaje</p>
-        `
-  })
-
-}
+    from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+    to: email,
+    subject: "UpTask - Reestablecé tu Contraseña",
+    text: "Reestablecé tu Contraseña",
+    html: `<p>Hola: ${nombre} Solicitaste reestablecer tu contraseña</p>
+    <p>Seguí el siguiente enlace para generar una nueva contraseña: 
+    <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer contraseña</a>
+    
+    <p>Si tu solicitaste este email, podés ignorar el mensaje</p>
+    
+    
+    `,
+  });
+};
